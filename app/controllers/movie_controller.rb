d0760eb9a58dfde513276e7f0a5a9c8ff7373f45
railsonn <railsongamer99@gmail.com>
2026-02-09 13:02:25 -0300

    resolving conflict routes

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,48 +1,22 @@
 class MovieController < ApplicationController
   def index
-    @movies = Movie.all
   end
 
   def create
-    @movie = Movie.new(movie_params)
-
-    if @movie.save
-      redirect_to @movie, notice: "Filme criado com sucesso."
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
   def destroy
-    @movie.destroy
-    redirect_to movies_path, notice: "Filme removido com sucesso."
   end
 
   def update
-    if @movie.update(movie_params)
-      redirect_to @movie, notice: "Filme atualizado com sucesso."
-    else
-      render :edit, status: :unprocessable_entity
-    end
   end
 
   def show
   end
 
   def new
-    @movie = Movie.new
   end
 
   def edit
-  end
-
-  private 
-
-  def set_movie
-    @movie = Movie.find(params[:id])
-  end
-
-  def movie_params
-    params.require(:movie).permit(:title, :overview, :release_date)
   end
 end
