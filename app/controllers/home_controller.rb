9fcce40c667d30492b80db94f8cdfe2d396b9de8
railsonn <railsongamer99@gmail.com>
2026-02-11 14:16:02 -0300

    implemented algoritmo of verify before database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,25 @@ class HomeController < ApplicationController
   def index
     page = params[:page] || 1
     title = params[:q]
-    @movies = @summary_service.general(title, page)
-    Movie.save_from_api(@movies)
+  
+    if params[:title].present?
+      # 1️⃣ Busca no banco primeiro
+      @movies = Movie.where(" title ILIKE?", "%#{params[:title]}%")
+
+      # 2️⃣ Se não encontrar nada, busca na API
+      if @movies.empty?
+        @movies = @summary_service.general(title, page)
+        Movie.save_from_api(api_movies)
+
+        @movies = Movie.where("title ILIKE ?", "%#{params[:title]}%")
+      end
+    else 
+      @movies = Movie.all.limit(20)
+    end
+    
     @next_movies = @summary_service.general(title, page.to_i + 1)
+
+    # Movie.save_from_api(@movies)
   end
 
   private
