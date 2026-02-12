f211f81a7edf997d637bbbfd65296770b37c828b
railsonn <railsongamer99@gmail.com>
2026-02-12 11:30:24 -0300

    create a function to ostruct

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
-  before_action :set_global_summary_service
   require "ostruct"
+  before_action :set_global_summary_service
 
   def index
     page = params[:page] || 1
@@ -15,31 +15,31 @@ class HomeController < ApplicationController
       if @movies.empty?
         @movies = @summary_service.general(title, page)
 
-        @movies = @movies.map do |api_movie|
-          OpenStruct.new(
-            title: api_movie["Title"],
-            year: api_movie["Year"],
-            poster: api_movie["Poster"],
-            movie_type: api_movie["Type"],
-            imdb_id: api_movie["imdbID"]
-          )
-        end
         Movie.save_from_api(@movies)
+        transform_requests_result(@movies)
       end
     else 
-      @movies = @summary_service.general(title, page)
 
-      @movies = @movies.map do |api_movie|
-        OpenStruct.new(
-          title: api_movie["Title"],
-          year: api_movie["Year"],
-          poster: api_movie["Poster"],
-          imdb_id: api_movie["imdbID"]
-        )
-      end
+      @movies = Movie.all.limit(10)
     end
 
-    @next_movies = @summary_service.general(title, page.to_i + 1)
+    @movies = @summary_service.general(title, page)
+    transform_requests_result(@movies)
+
+    # @count_movies = Movie.count / 10
+    # @next_movies = Movie.all.limit(10).offset(10)
+  end
+
+  def transform_requests_result(movies)
+    @movies = movies.map do |api_movie|
+      OpenStruct.new(
+        title: api_movie["Title"],
+        year: api_movie["Year"],
+        poster: api_movie["Poster"],
+        movie_type: api_movie["Type"],
+        imdb_id: api_movie["imdbID"]
+      )
+    end
   end
 
   private
