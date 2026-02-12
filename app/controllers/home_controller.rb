1133d834ac40cca46e0b3cbe19983b299e051803
railsonn <railsongamer99@gmail.com>
2026-02-12 00:16:38 -0300

    resolving conflict helper. add ostruct and resolving

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
   before_action :set_global_summary_service
+  require "ostruct"
 
   def index
     page = params[:page] || 1
     title = params[:q]
   
+    # se tiver titulo 
     if title.present?
       # 1️⃣ Busca no banco primeiro
       @movies = Movie.where("title LIKE?", "%#{title}%").limit(12)
@@ -12,17 +14,31 @@ class HomeController < ApplicationController
       # 2️⃣ Se não encontrar nada, busca na API
       if @movies.empty?
         @movies = @summary_service.general(title, page)
+
+        @movies = @movies.map do |api_movie|
+          OpenStruct.new(
+            title: api_movie["Title"],
+            year: api_movie["Year"],
+            poster: api_movie["Poster"],
+            movie_type: api_movie["Type"],
+            imdb_id: api_movie["imdbID"]
+          )
+        end
         Movie.save_from_api(@movies)
       end
     else 
-      puts "#{Movie.count } =================="
       @movies = @summary_service.general(title, page)
-      # else
-      #   puts "#{Movie.count} ==============================================="
-      #   @movies = Movie.all.limit(20)
-      # end
+
+      @movies = @movies.map do |api_movie|
+        OpenStruct.new(
+          title: api_movie["Title"],
+          year: api_movie["Year"],
+          poster: api_movie["Poster"],
+          imdb_id: api_movie["imdbID"]
+        )
+      end
     end
-    
+
     @next_movies = @summary_service.general(title, page.to_i + 1)
   end
 
