17bac5cdf4f0ae12de9aec2f4550d627b2665602
railsonn <railsongamer99@gmail.com>
2026-02-14 11:10:38 -0300

    link_to to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,25 @@ class CategoriesController < ApplicationController
     @movies = @summary_service.categories(10751)
     transform_requests_result(@movies)
   end
+  
+    def transform_requests_result(movies)
+    @movies = movies.map do |api_movie|
+      base_url = "https://image.tmdb.org/t/p/w500"  
+      poster_path = api_movie["poster_path"]
+      poster_url = "#{base_url}#{poster_path}"
+
+      OpenStruct.new(
+        title: api_movie["original_title"],
+        year: api_movie["release_date"],
+        poster: poster_url,
+        genre: api_movie["genre_ids"],
+        vote_average: api_movie["vote_average"],
+        adult: api_movie["adult"],
+        overview: api_movie["overview"],
+        id_movie: api_movie["id"]
+      )
+    end
+  end
 
 
 
