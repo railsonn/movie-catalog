7e7488b888d934bdc9f276c1349177619d929d8c
railsonn <railsongamer99@gmail.com>
2026-03-06 16:53:19 -0300

    feat: add methods in sevice

--- app/services/movie/all_movies.rb
+++ app/services/movie/all_movies.rb
@@ -1,18 +1,37 @@
-  def general(title, page)
-    results = []
+ module Movie
+  class AllMoviesService
+    def general(title, page)
+      results = []
 
-    if title.blank?
-      CURRENT_TITLES.each do |search_term|
-        response = TmdbService.search_recent(search_term, page)
+      if title.blank?
+        CURRENT_TITLES.each do |search_term|
+          response = TmdbService.search_recent(search_term, page)
+          results += response["results"] if valid_response?(response)
+        end
+      else
+        response = TmdbService.search(title, page)
         results += response["results"] if valid_response?(response)
       end
-    else
-      response = TmdbService.search(title, page)
-      results += response["results"] if valid_response?(response)
+
+      unique_results = results.uniq { |movie| movie["id"]}
+
+      # funcao para atribuir os generos em string para cada id do genre_ids
+      unique_results.each{ |result| format_genres(result) }
     end
 
-    unique_results = results.uniq { |movie| movie["id"]}
+    private 
 
-    # funcao para atribuir os generos em string para cada id do genre_ids
-    unique_results.each{ |result| format_genres(result) }
+    def valid_response?(response)
+      response && response["results"] && !response["results"].empty?
+    end
+
+    def format_genres(result)
+      result["genre_ids"].each_with_index do |genre_id, i| 
+        result["genre_ids"][i] = TMDB_MOVIE_GENRES[genre_id]
+      end
+      .compact
+      .join(", ")
+    end
+    
   end
+end
