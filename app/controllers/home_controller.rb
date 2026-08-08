9554ae1fbb119f3ef04a15801a44eb93182eed64
railsonn <railsongamer99@gmail.com>
2026-08-08 15:10:31 -0300

    fix:  Removed method transform_requests_result and added access to it in the service.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,26 +34,6 @@ class HomeController < ApplicationController
     transform_requests_result(@next_movies)
   end
 
-  # Transforma os resultados da API em objetos
-  def transform_requests_result(movies)
-    @movies = movies.map do |api_movie|
-      base_url = "https://image.tmdb.org/t/p/w500"
-      poster_path = api_movie["poster_path"]
-      poster_url = "#{base_url}#{poster_path}"
-
-      OpenStruct.new(
-        title: api_movie["original_title"],
-        year: api_movie["release_date"],
-        poster: poster_url,
-        genre: api_movie["genre_ids"],
-        vote_average: api_movie["vote_average"],
-        adult: api_movie["adult"],
-        overview: api_movie["overview"],
-        id_movie: api_movie["id"]
-      )
-    end
-  end
-
   private
 
   # Inicializa o serviço de filmes
@@ -64,5 +44,9 @@ class HomeController < ApplicationController
   # Inicializa o serviço de resumo
   def set_global_summary_service
     @summary_service = GlobalSummary.new
+  end
+  
+  def transform_requests_result(movies)
+    @movies = MovieTransformer.transform_requests_result(movies)
   end
 end
