0fdd77a1cb949818489c27226957a04459c17a47
railsonn <railsongamer99@gmail.com>
2026-08-08 15:13:58 -0300

    feat: add comments explicit in helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # metodo para formatar o email do usuario, pegando apenas a parte antes do @ e capitalizando a primeira letra
   def format_email(email)
     email.split('@').first.capitalize 
   end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,5 @@
 module HomeHelper
+  # metodo para formatar o genero do filme, pegando apenas a parte antes do " e substituindo por ,
   def format_text(movie)
     movie.genre.split('"').join(', ')
   end
